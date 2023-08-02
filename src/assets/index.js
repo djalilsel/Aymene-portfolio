2f3334489630e8025ff10ee011babcafb6b87bb4
djalil <selamnia.a.eldjalil@gmail.com>
2023-08-02 09:28:49 +0100

    experience + tech sections done

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,22 +9,19 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
+import ansible from "./tech/ansible.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import aws from "./tech/aws.png";
+import bashcripting from "./tech/bashscripting.png";
+import kubernetes from "./tech/kubernetes.png";
+import python from "./tech/python.png";
+import SQL from "./tech/SQL.png";
+import vmware from "./tech/vmware.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import numeryx from './company/numeryx.png'
+import L from './company/L.png'
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -41,21 +38,18 @@ export {
   close,
   css,
   docker,
-  figma,
+  ansible,
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  aws,
+  bashcripting,
+  kubernetes,
+  python,
+  SQL,
+  vmware,
+  numeryx,
+  L,
   carrent,
   jobit,
   tripguide,
